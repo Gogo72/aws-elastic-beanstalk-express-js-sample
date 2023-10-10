1f700619ec745c9725741ae4f52c558ad561ed44
Tomas Prasil <tomas.prasil@student.curtin.gov.au>
2023-10-10 09:02:15 +0800

    updated app3000.js with message

--- app3000.js
+++ app3000.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('ISEC6000, Assignment2, Testing js application in Jenkis Pipeline'));
 
 app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+console.log(`App running for 30 seconds on http://localhost:${port}`);
 
 setTimeout((function() {  
     return process.exit(0);
