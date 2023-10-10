9dcc7fe93bcfdfb9ca45639d8c190a66167b3a84
Tomas Prasil <tomas.prasil@student.curtin.gov.au>
2023-10-10 10:38:08 +0800

    updated app3000.js correcting typo

--- app3000.js
+++ app3000.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('ISEC6000, Assignment2, Testing js application in Jenkis Pipeline'));
+app.get('/', (req, res) => res.send('ISEC6000, Assignment2, Testing js application in Jenkins Pipeline'));
 
 app.listen(port);
 console.log(`App running for 30 seconds on http://localhost:${port}`);
